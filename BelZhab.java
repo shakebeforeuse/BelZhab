7c6ee63b8e1261c0ccaf38e5aeede544eac256e9
Manuel F <shakebeforeuse@gmail.com>
2015-03-16 13:28:50 +0100

    Unido todo en un solo fichero

--- BelZhab.java
+++ BelZhab.java
@@ -3,8 +3,9 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.Random;
+import java.util.Scanner;
 
-public class BelZhab
+public class BelZhab implements Runnable
 {
 	private Random random;
 
@@ -25,6 +26,9 @@ public class BelZhab
 	private static ExecutorService threadPool;
 	private static Runnable[] tareas;
 	public  static CyclicBarrier barrera;
+	
+	private int inicio;
+	private int fin;
 	
 	
 	public BelZhab(int ancho, int alto, double alfa, double beta, double gamma, int nTareas)
@@ -47,6 +51,12 @@ public class BelZhab
 	public BelZhab(int ancho, int alto)
 	{
 		this(ancho, alto, 1.2, 1, 1, Runtime.getRuntime().availableProcessors());
+	}
+	
+	private BelZhab(int inicio, int fin, boolean dummy)
+	{
+		this.inicio = inicio;
+		this.fin    = fin;
 	}
 	
 	public void aleatorio()
@@ -82,7 +92,7 @@ public class BelZhab
 			if ((i+1) == nucleos)
 				finIntervalo = alto;
 			
-			tareas[i] = new BelZhabWorker(inicioIntervalo, finIntervalo);
+			tareas[i] = new BelZhab(inicioIntervalo, finIntervalo, false);
 		}
 	}
 	
@@ -117,5 +127,149 @@ public class BelZhab
 	{		
 		while (!threadPool.isTerminated())
 			threadPool.shutdown();
+	}
+	
+	public void run()
+	{
+		double[] concentracion;
+		
+		for (int i = inicio; i < fin; ++i)
+		{
+			for (int j = 0; j < ancho; ++j)
+			{
+				concentracion = concentraciones(j, i);
+				
+				reaccion[(generacion+1) % 2][0][i][j] = parteFlotante(concentracion[0] * (1 + (alfa  * concentracion[1] - gamma * concentracion[2])));
+				reaccion[(generacion+1) % 2][1][i][j] = parteFlotante(concentracion[1] * (1 + (beta  * concentracion[2] - alfa  * concentracion[0])));
+				reaccion[(generacion+1) % 2][2][i][j] = parteFlotante(concentracion[2] * (1 + (gamma * concentracion[0] - beta  * concentracion[1])));
+			}
+		}
+		
+		try
+		{
+			barrera.await();
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("InterruptedException: run()");
+			System.out.println("Error: " + e.getMessage());
+		}
+		catch (BrokenBarrierException e)
+		{
+			System.out.println("BrokenBarrierException: run()");
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	private static double[] concentraciones(int x, int y)
+	{
+		double[] concentracion = new double[3];
+		
+		for (int i = -1; i <= 1; ++i)
+		{
+			for (int j = -1; j <= 1; ++j)
+			{
+				concentracion[0] += reaccion[generacion % 2][0][mod(y+i, alto)][mod(x+j, ancho)];
+				concentracion[1] += reaccion[generacion % 2][1][mod(y+i, alto)][mod(x+j, ancho)];
+				concentracion[2] += reaccion[generacion % 2][2][mod(y+i, alto)][mod(x+j, ancho)];
+			}
+		}
+		
+		concentracion[0] /= 9;
+		concentracion[1] /= 9;
+		concentracion[2] /= 9;
+		
+		return concentracion;
+	}
+	
+	private static double parteFlotante(double f)
+	{
+		return f - Math.floor(f);
+	}
+	
+	private static int mod(int a, int b)
+	{		
+		return (a + b) % b;
+	}
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		int tam = 0;
+		int tareas = 0;
+		int iteraciones = 0;
+		
+		double alfa  = 0;
+		double beta  = 0;
+		double gamma = 0;
+		double tic   = 0;
+		double tac   = 0;
+		
+		
+		if (args.length >= 2)
+		{
+			tam         = Integer.parseInt(args[0]);
+			iteraciones = Integer.parseInt(args[1]);
+			tareas      = 0;
+			
+			alfa   = 1.2;
+			beta   = 1.0;
+			gamma  = 1.0;
+		}
+		else
+		{
+			if (args.length >= 3)
+				tareas = Integer.parseInt(args[2]);
+			else
+			{
+				if (args.length == 6)
+				{
+					alfa  = Double.parseDouble(args[3]);
+					beta  = Double.parseDouble(args[4]);
+					gamma = Double.parseDouble(args[5]);
+				}
+				else	
+				{
+					Scanner teclado = new Scanner(System.in);
+					
+					System.out.println("Uso: java BelZhab <Tamaño> <Iteraciones> [<Tareas> [<alfa> <beta> <gamma>]]");
+					
+					System.out.println("Introduce un tamaño válido");
+					tam = teclado.nextInt();
+					System.out.println("Introduce el número de generaciones a computar");
+					iteraciones = teclado.nextInt();
+					System.out.println("Introduce el número de tareas que se usarán");
+					tareas = teclado.nextInt();
+					
+					System.out.println("Introduce el coeficiente alfa");
+					alfa = teclado.nextDouble();
+					System.out.println("Introduce el coeficiente beta");
+					beta = teclado.nextDouble();
+					System.out.println("Introduce el coeficiente gamma");
+					gamma = teclado.nextDouble();
+					
+					teclado.close();
+				}
+			}
+		}
+		
+		if (tareas < 1)
+			tareas = Runtime.getRuntime().availableProcessors();
+
+		BelZhab reaccion = new BelZhab(tam, tam, alfa, beta, gamma, tareas);
+			
+		tic = System.currentTimeMillis();
+		
+		for (int i = 0; i < iteraciones; ++i)
+			reaccion.siguienteGeneracion();
+			
+		tac = System.currentTimeMillis();
+		
+		
+		System.out.println("Tiempo medio: " + (tac-tic)/iteraciones + " ms.");
+		
+		reaccion.close();
 	}
 }
